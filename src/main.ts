cd98d829aba826f95861745864f6e394b7337af0
Phan Van Tien <119719148+phanvantien2907@users.noreply.github.com>
2025-10-23 22:13:45 +0700

    Refactor NestJS bootstrap with Express adapter

--- src/main.ts
+++ src/main.ts
@@ -3,35 +3,44 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-declare const module: any;
+const server = express();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-   app.useGlobalPipes(new ValidationPipe());
-   app.enableCors({
-     origin: '*',
-     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-     preflightContinue: false,
-   });
-   app.setGlobalPrefix('api');
-   const config = new DocumentBuilder()
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
     .setTitle('API quản lý chi tiêu ngân sách cho Nhóm/Gia đình')
-    .setDescription('Đây là api cho hệ thống quản lý các chi tiêu về ngân sách cho nhóm và gia đình')
+    .setDescription('Đây là API cho hệ thống quản lý chi tiêu nhóm/gia đình')
     .setVersion('1.0')
-    .addBearerAuth({
-      type: 'http',
-      scheme: 'bearer',
-       bearerFormat: 'JWT',
-       name: 'Authorization',
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
         in: 'header',
-    }, 'access-token')
+      },
+      'access-token',
+    )
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
-  if(module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
